bd4250552db173eaff7de1236baac53fbbe87516
Gunn-Treepaech <treepaech@kkumail.com>
2024-02-05 18:58:45 +0700

    add new db

--- Docker swarm/Docker Stacks/Example With Mysql/init.sql
+++ Docker swarm/Docker Stacks/Example With Mysql/init.sql
@@ -4,10 +4,10 @@ CREATE TABLE IF NOT EXISTS example_data (
     test1 VARCHAR(50),
     test2 DECIMAL(4, 2),
     test3 DECIMAL(5, 2),
-    PRIMARY KEY (bank_name)
+    PRIMARY KEY (test1)
 );
 
-INSERT INTO interest_rates (test1, test2, test3) VALUES
+INSERT INTO example_data (test1, test2, test3) VALUES
     ('SCB', 0, 7.300),
     ('UOB', 0, 8.800),
     ('KTB', 0, 7.570),
