2e24fc5e7d46658e53274ead7666f5534ed39c35
Gunn-Treepaech <treepaech@kkumail.com>
2024-02-05 19:29:22 +0700

    add new function

--- Docker swarm/Docker Stacks/Example With Mysql/app.py
+++ Docker swarm/Docker Stacks/Example With Mysql/app.py
@@ -1,24 +1,42 @@
-from flask import Flask, jsonify
+from flask import Flask, request, jsonify
 import mysql.connector
+import json
 
 app = Flask(__name__)
+
+config = {
+    'user': 'user',
+    'password': 'user',
+    'host': 'db',
+    'port': '3306',
+    'database': 'example'
+}
 
 @app.route('/', methods=['GET'])
 def home():
     return 'Example Docker Stack With Mysql'
 
 @app.route('/db', methods=['GET'])
-def showdatadb():
-    return jsonify(getdatadb())
+def showdb():
+    return jsonify(getdb())
 
-def getdatadb():
-    config = {
-        'user': 'user',
-        'password': 'user',
-        'host': 'db',
-        'port': '3306',
-        'database': 'example'
-    }
+@app.route('/adddata', methods=['POST'])
+def adddata():
+    datas = request.json
+    insertdata(**datas)
+    return jsonify(getdb())
+
+@app.route('/addcolumn', methods=['GET'])
+def addcolumn():
+    insertcolumn()
+    return jsonify(getdb())
+
+@app.route('/addtable', methods=['GET'])
+def addtable():
+    inserttable()
+    return
+
+def getdb():
     connection = mysql.connector.connect(**config)
     cursor = connection.cursor(dictionary=True)
     cursor.execute('SELECT * FROM example_data;')
@@ -27,19 +45,51 @@ def getdatadb():
     connection.close()
     return results
 
+def insertdata(**datas):
+    data_to_insert = datas
+    connection = mysql.connector.connect(**config)
+    cursor = connection.cursor(dictionary=True)
+    insert_query = "INSERT INTO example_data (test1, test2, test3) VALUES (%s, %s, %s)"
+    for entry in data_to_insert:
+        cursor.execute(insert_query, (json.dumps(entry['test1']), entry['test2'], entry['test3']))
+    connection.commit()
+    cursor.close()
+    connection.close()
+    return
+
+def insertcolumn():
+    connection = mysql.connector.connect(**config)
+    cursor = connection.cursor(dictionary=True)
+    alter_table_query = "ALTER TABLE example_data ADD COLUMN new_column VARCHAR(50)"
+    cursor.execute(alter_table_query)
+    connection.commit()
+    cursor.close()
+    connection.close()
+    return
+
+def inserttable():
+    connection = mysql.connector.connect(**config)
+    cursor = connection.cursor(dictionary=True)
+    create_table_query = """
+    CREATE TABLE IF NOT EXISTS new_table (
+    id INT AUTO_INCREMENT PRIMARY KEY,
+    column1 VARCHAR(50),
+    column2 DECIMAL(4, 2),
+    column3 DECIMAL(5, 2)
+    )
+    """
+    cursor.execute(create_table_query)
+    connection.commit()
+    cursor.close()
+    connection.close()
+    return
+
 if __name__ == '__main__':
     # -----------------------------------------------------------------------------------------------
     config = {
-    "start_month": 11,
-    "start_year": 2024,
-    "initial_loan": 100000000,
-    "fixed_interest": 2.95,
-    "fixed_year": 3,
-    "MRR": 8.8,
-    "monthly_payment": 15000,
-    "chang_interest": [2.95, 1.95],
-    "bank": "UOB"
+    "test1": 'gun',
+    "test2": 2024,
+    "test3": 100,
 }
     # ------------------------------------------------------------------------------------------------
-    #calculate_loan_schedule(**config)
     app.run(host='0.0.0.0', port=12345)
